faddac00a6b345c2dd006bc0b70d939a2ac406ac
Steven Rivera <riverasteven9966@gmail.com>
2026-05-22 00:08:50 -0500

    Pagina detalles de producto

--- store/views.py
+++ store/views.py
@@ -5,3 +5,6 @@ def home(request):
     productos = Producto.objects.filter(disponible=True)
     contexto = {'productos': productos}
     return render(request, 'store/home.html', contexto)
+
+def product(request):
+    return render(request, 'store/product.html')
